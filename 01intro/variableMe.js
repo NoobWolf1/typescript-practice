5330bf93040010c4c137f39fc42711317bd3a81e
Malay Shukla <malayshukla@Malays-MacBook-Air.local>
2025-05-02 17:08:24 +0530

    basic primitive types and type inference

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Malay";
 console.log(greetings);
+// number
+var userId = 334466;
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -2,4 +2,16 @@ let greetings: string = "Hello Malay";
 
 console.log(greetings)
 
+// number
+let userId: number = 334466;
+userId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false;
+
+
+// type inference, here we dont directly add number(type to the ts code ie.,)
+// let userId: number = 334466;
+// userId.toFixed();
+
 export {};
