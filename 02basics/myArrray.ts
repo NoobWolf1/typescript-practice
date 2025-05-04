9e778ee9c53810233dd1a7e3573a5baf7366a0d6
Malay Shukla <malayshukla@Malays-MacBook-Air.local>
2025-05-04 17:24:03 +0530

    basics of array

---
+++ 02basics/myArrray.ts
@@ -0,0 +1,16 @@
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type  User = {
+    name: string,
+    isActive: boolean
+}
+
+const allUsers: User[] = [];
+
+const MLModels: number[][] = [[255,255,255], [23]];
+superHeroes.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({name: "", isActive: true});
