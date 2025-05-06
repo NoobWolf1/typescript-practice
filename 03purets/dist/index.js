db8becb95dfb8a5ca6b61b85ce7f6fd0a99762de
Malay Shukla <malayshukla@Malays-MacBook-Air.local>
2025-05-06 11:24:25 +0530

    private and public in Ts

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,10 +1,23 @@
 "use strict";
+// class User {
+//       email: string;
+//     private name: string;
+//     readonly city: string = "";
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+// malay.city = "Jabalpur"
+// if you dont even want to access the city then use Private
+// default is public
+// equivalent of above code is 
 class User {
-    constructor(email, name) {
-        this.city = "";
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this.city = "Jabalpur";
     }
 }
-const malay = new User("ms@ms.com", "Malay");
-// malay.city = "Jabalpur"
+const malay = new User("ms@ms.com", "Malay", "2");
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,14 +1,28 @@
+// class User {
+//       email: string;
+//     private name: string;
+//     readonly city: string = "";
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name;
+//     }
+// }
+
+
+
+// malay.city = "Jabalpur"
+// if you dont even want to access the city then use Private
+// default is public
+
+// equivalent of above code is 
 class User {
-    email: string;
-    name: string;
-    readonly city: string = "";
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name;
+    readonly city: string = "Jabalpur";
+    constructor(public email: string,
+        public name: string,
+        private userId: string) {
     }
 }
 
 
-const malay = new User("ms@ms.com", "Malay");
+const malay = new User("ms@ms.com", "Malay", "2");
 
-// malay.city = "Jabalpur"
